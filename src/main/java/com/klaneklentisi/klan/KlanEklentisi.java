4cf8c2b047966ffceeae11d8e92313ad4c09bb9d
Furkan Yılmaz <klavyem55@gmail.com>
2026-07-30 00:58:01 +0300

    KlanEklentisi güncelleme: src/main/java/com/klaneklentisi/klan/KlanEklentisi.java [skip ci]

--- src/main/java/com/klaneklentisi/klan/KlanEklentisi.java
+++ src/main/java/com/klaneklentisi/klan/KlanEklentisi.java
@@ -2,11 +2,16 @@ package com.klaneklentisi.klan;
 
 import com.klaneklentisi.klan.command.KlanKomutu;
 import com.klaneklentisi.klan.command.KlanYonetimKomutu;
+import com.klaneklentisi.klan.istatistik.IstatistikDeposu;
+import com.klaneklentisi.klan.istatistik.IstatistikYoneticisi;
+import com.klaneklentisi.klan.istatistik.YamlIstatistikDeposu;
+import com.klaneklentisi.klan.listener.OlumDinleyici;
 import com.klaneklentisi.klan.listener.SohbetDinleyici;
 import com.klaneklentisi.klan.manager.KlanYoneticisi;
 import com.klaneklentisi.klan.storage.KlanDeposu;
 import com.klaneklentisi.klan.storage.YamlKlanDeposu;
 import com.klaneklentisi.klan.util.Mesajlar;
+import com.klaneklentisi.klan.util.VaultEkonomi;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,6 +23,9 @@ public class KlanEklentisi extends JavaPlugin {
     private Mesajlar mesajlar;
     private KlanDeposu depo;
     private KlanKomutu klanKomutu;
+    private IstatistikYoneticisi istatistikYoneticisi;
+    private IstatistikDeposu istatistikDeposu;
+    private VaultEkonomi vaultEkonomi;
     private final com.klaneklentisi.klan.gui.GirdiYoneticisi girdiYoneticisi = new com.klaneklentisi.klan.gui.GirdiYoneticisi();
 
     @Override
@@ -30,6 +38,12 @@ public class KlanEklentisi extends JavaPlugin {
 
         klanYoneticisi.yukle();
 
+        this.istatistikDeposu = new YamlIstatistikDeposu(this);
+        this.istatistikYoneticisi = new IstatistikYoneticisi(this, istatistikDeposu);
+        istatistikYoneticisi.yukle();
+
+        this.vaultEkonomi = new VaultEkonomi(this);
+
         this.klanKomutu = new KlanKomutu(this);
         getCommand("klan").setExecutor(klanKomutu);
         getCommand("klan").setTabCompleter(klanKomutu);
@@ -40,10 +54,14 @@ public class KlanEklentisi extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new SohbetDinleyici(this, klanKomutu), this);
         getServer().getPluginManager().registerEvents(new com.klaneklentisi.klan.gui.GuiDinleyici(), this);
+        getServer().getPluginManager().registerEvents(new OlumDinleyici(this), this);
 
         if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             new com.klaneklentisi.klan.util.KlanPlaceholder(this).register();
             getLogger().info("PlaceholderAPI bulundu, klan placeholderları etkinleştirildi.");
+        }
+        if (vaultEkonomi.aktifMi()) {
+            getLogger().info("Vault bulundu, öldürme ödüllerinde para desteği etkinleştirildi.");
         }
 
         getLogger().info("KlanEklentisi etkinleştirildi.");
@@ -56,6 +74,9 @@ public class KlanEklentisi extends JavaPlugin {
         }
         if (depo != null) {
             depo.kapat();
+        }
+        if (istatistikYoneticisi != null) {
+            istatistikYoneticisi.kaydet();
         }
         getLogger().info("KlanEklentisi devre dışı bırakıldı, tüm klan verileri kaydedildi.");
     }
@@ -70,6 +91,14 @@ public class KlanEklentisi extends JavaPlugin {
 
     public KlanKomutu getKlanKomutu() {
         return klanKomutu;
+    }
+
+    public IstatistikYoneticisi getIstatistikYoneticisi() {
+        return istatistikYoneticisi;
+    }
+
+    public VaultEkonomi getVaultEkonomi() {
+        return vaultEkonomi;
     }
 
     public com.klaneklentisi.klan.gui.GirdiYoneticisi getGirdiYoneticisi() {

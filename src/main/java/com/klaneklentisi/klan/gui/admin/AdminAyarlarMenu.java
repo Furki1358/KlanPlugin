97e552154dfcc082181d1210e22979ddff40a2d8
Furkan Yılmaz <klavyem55@gmail.com>
2026-07-31 10:54:12 +0300

    KlanEklentisi güncelleme: src/main/java/com/klaneklentisi/klan/gui/admin/AdminAyarlarMenu.java [skip ci]

--- src/main/java/com/klaneklentisi/klan/gui/admin/AdminAyarlarMenu.java
+++ src/main/java/com/klaneklentisi/klan/gui/admin/AdminAyarlarMenu.java
@@ -18,7 +18,7 @@ public class AdminAyarlarMenu extends Menu {
 
     @Override
     protected int boyut() {
-        return 36;
+        return 54;
     }
 
     @Override
@@ -26,11 +26,16 @@ public class AdminAyarlarMenu extends Menu {
         return mesajlar.baslik("admin.ayarlar.baslik");
     }
 
+    private void dolduruCam(int... slotlar) {
+        for (int s : slotlar) {
+            envanter.setItem(s, Esya.olustur(Material.WHITE_STAINED_GLASS_PANE, " "));
+        }
+    }
+
     @Override
     protected void doldur() {
-        for (int i = 0; i < boyut(); i++) {
-            envanter.setItem(i, Esya.doldurucu());
-        }
+        kenarCiz();
+        dolduruCam(13, 19, 23, 24, 25, 28, 29, 30, 32, 33, 34);
 
         boolean tekKlan = eklenti.getConfig().getBoolean("genel.tek-klan-siniri", true);
         boolean usAktif = eklenti.getConfig().getBoolean("us-sistemi.aktif", true);
@@ -42,21 +47,23 @@ public class AdminAyarlarMenu extends Menu {
                 mesajlar.baslik("admin.ayarlar.tek-klan-siniri", Map.of("deger", tekKlan ? "Açık" : "Kapalı")),
                 List.of(mesajlar.baslik("admin.ayarlar.tek-klan-siniri-aciklama"))));
 
-        envanter.setItem(12, Esya.olustur(Material.RED_BED,
+        envanter.setItem(11, Esya.olustur(Material.RED_BED,
                 mesajlar.baslik("admin.ayarlar.us-aktif", Map.of("deger", usAktif ? "Açık" : "Kapalı")),
                 List.of(mesajlar.baslik("admin.ayarlar.us-aktif-aciklama"))));
 
-        envanter.setItem(14, Esya.olustur(Material.CLOCK,
+        envanter.setItem(12, Esya.olustur(Material.CLOCK,
                 mesajlar.baslik("admin.ayarlar.us-bekleme", Map.of("deger", String.valueOf(usBekleme))),
                 List.of(mesajlar.baslik("admin.ayarlar.us-bekleme-aciklama"))));
 
-        envanter.setItem(19, Esya.olustur(Material.NAME_TAG,
+        envanter.setItem(14, Esya.olustur(Material.NAME_TAG,
                 mesajlar.baslik("admin.ayarlar.min-isim", Map.of("deger", String.valueOf(minIsim))),
                 List.of(mesajlar.baslik("admin.ayarlar.uzunluk-aciklama"))));
 
-        envanter.setItem(21, Esya.olustur(Material.NAME_TAG,
+        envanter.setItem(15, Esya.olustur(Material.NAME_TAG,
                 mesajlar.baslik("admin.ayarlar.maks-isim", Map.of("deger", String.valueOf(maksIsim))),
                 List.of(mesajlar.baslik("admin.ayarlar.uzunluk-aciklama"))));
+
+        envanter.setItem(16, Esya.olustur(Material.EMERALD, mesajlar.baslik("admin.ana.yenile")));
 
         envanter.setItem(31, Esya.olustur(Material.ARROW, mesajlar.baslik("admin.ayarlar.geri")));
     }
@@ -70,15 +77,22 @@ public class AdminAyarlarMenu extends Menu {
                 eklenti.saveConfig();
                 yenile();
             }
-            case 12 -> {
+            case 11 -> {
                 boolean mevcut = eklenti.getConfig().getBoolean("us-sistemi.aktif", true);
                 eklenti.getConfig().set("us-sistemi.aktif", !mevcut);
                 eklenti.saveConfig();
                 yenile();
             }
-            case 14 -> sayiIste("us-sistemi.bekleme-suresi", 0, 300);
-            case 19 -> sayiIste("genel.min-isim-uzunlugu", 1, 32);
-            case 21 -> sayiIste("genel.maks-isim-uzunlugu", 1, 32);
+            case 12 -> sayiIste("us-sistemi.bekleme-suresi", 0, 300);
+            case 14 -> sayiIste("genel.min-isim-uzunlugu", 1, 32);
+            case 15 -> sayiIste("genel.maks-isim-uzunlugu", 1, 32);
+            case 16 -> {
+                eklenti.reloadConfig();
+                mesajlar.yukle();
+                eklenti.getKomutAyarlari().yukle();
+                oyuncu.sendMessage(mesajlar.al("yonetim.yenilendi"));
+                yenile();
+            }
             case 31 -> new AdminAnaMenu(eklenti, oyuncu).ac();
             default -> {}
         }

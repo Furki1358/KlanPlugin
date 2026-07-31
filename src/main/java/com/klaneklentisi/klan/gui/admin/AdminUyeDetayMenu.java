b378f83f0de1b9fa9b8616163d006dfb342f17ab
Furkan Yılmaz <klavyem55@gmail.com>
2026-08-01 00:43:09 +0300

    KlanEklentisi güncelleme: src/main/java/com/klaneklentisi/klan/gui/admin/AdminUyeDetayMenu.java [skip ci]

--- src/main/java/com/klaneklentisi/klan/gui/admin/AdminUyeDetayMenu.java
+++ src/main/java/com/klaneklentisi/klan/gui/admin/AdminUyeDetayMenu.java
@@ -46,7 +46,7 @@ public class AdminUyeDetayMenu extends Menu {
 
     private void dolduruCam(int... slotlar) {
         for (int s : slotlar) {
-            envanter.setItem(s, Esya.olustur(Material.WHITE_STAINED_GLASS_PANE, " "));
+            envanter.setItem(s, Esya.olustur(Material.BLACK_STAINED_GLASS_PANE, " "));
         }
     }
 
